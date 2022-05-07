263a4ac7ef2df42f601d9862a6d27886dc444957
zhanghan <Lancelot_Lewis@163.com>
2022-05-07 15:06:01 +0800

    feat: add @did-connect/vue -> logo component

---
+++ packages/vue/src/components/logo/logo.vue
@@ -0,0 +1,21 @@
+<script setup>
+import DidLogoIcon from '@arcblock/icons/src/icons/did-logo.svg?component';
+
+const props = defineProps({
+  size: {
+    type: [Number, String],
+    default: 0,
+  },
+});
+const height = Number(props.size) > 0 ? `${Number(props.size)}px` : props.size;
+</script>
+
+<template>
+  <DidLogoIcon
+    :style="{
+      width: 'auto',
+      fill: 'currentColor',
+      height: height || '1em',
+    }"
+  />
+</template>
---
+++ packages/vue/stories/Logo.stories.js
@@ -0,0 +1,111 @@
+import Logo from '../src/components/logo';
+import 'uno.css';
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Logo',
+  component: Logo,
+  argTypes: {
+    size: {
+      control: {
+        type: 'number',
+      },
+    },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  components: { Logo },
+  setup: () => ({ args }),
+  template: `<Logo v-bind="args" />`,
+});
+
+export const CustomSizeNumber = Template.bind({});
+CustomSizeNumber.storyName = 'Custom Size (Number)';
+CustomSizeNumber.args = {
+  size: 40,
+};
+
+export const CustomSizeString = Template.bind({});
+CustomSizeString.storyName = 'Custom Size (String)';
+CustomSizeString.argTypes = {
+  size: {
+    control: {
+      type: 'text',
+    },
+  },
+};
+CustomSizeString.args = {
+  size: '4rem',
+};
+
+export const CustomStyle = (args) => ({
+  components: { Logo },
+  setup: () => ({ args }),
+  template: `<Logo v-bind="args" :size="64" :style="{color:args.color}"/>`,
+});
+CustomStyle.parameters = { controls: { exclude: ['size'] } };
+CustomStyle.storyName = 'Custom Style';
+CustomStyle.argTypes = {
+  color: {
+    control: {
+      type: 'color',
+      presetColors: ['red', 'green'],
+    },
+  },
+};
+CustomStyle.args = {
+  color: 'blue',
+};
+
+export const InheritStyle = (args) => ({
+  components: { Logo },
+  setup: () => ({ args }),
+  template: `
+  <div :style="args.style">
+    My color is green, and font size is 64px, so is the icon
+    <br />
+    <Logo v-bind="args"/>
+  </div>
+  `,
+});
+InheritStyle.parameters = { controls: { exclude: ['size'] } };
+InheritStyle.argTypes = {
+  style: {
+    control: {
+      type: 'object',
+    },
+  },
+};
+InheritStyle.args = {
+  style: {
+    color: 'green',
+    fontSize: '64px',
+    textAlign: 'center',
+  },
+};
+
+export const ComposeUse = (args) => ({
+  components: { Logo },
+  setup: () => ({ args }),
+  template: `
+  <div
+    :style="{
+      display: 'flex',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      padding: '32px',
+      borderRadius: '128px',
+      width: '192px',
+      height: '192px',
+      lineHeight: '192px',
+      background: '#4E6AF6',
+      color: '#ffffff',
+      margin: '32px',
+    }">
+    <Logo v-bind="args" :size="128" />
+  </div>
+  `,
+});
+ComposeUse.parameters = { controls: { exclude: ['size'] } };
