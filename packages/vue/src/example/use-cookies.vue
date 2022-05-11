a9ac22062d29ef06f4d5dc119b5aaa9c22351ce3
zhanghan <Lancelot_Lewis@163.com>
2022-05-11 22:44:42 +0800

    feat: add @did-connect/vue use-cookies hooks

---
+++ packages/vue/src/example/use-cookies.vue
@@ -0,0 +1,13 @@
+<script setup>
+import useCookies from '../hooks/use-cookies';
+
+const testCookie = useCookies('test_cookie', 'test-initialize');
+function changeCookie() {
+  testCookie.value = testCookie.value + '-';
+}
+</script>
+
+<template>
+  <button @click="changeCookie">change cookies</button>
+  <div>{{ testCookie }}</div>
+</template>
