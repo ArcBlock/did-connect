b86f19d66f8c58ec10b8a55f0818c9453d56e563
wangshijun <wangshijun2010@gmail.com>
2022-06-09 09:27:28 +0800

    chore: add more test case for util

--- core/state/tests/util.spec.js
+++ core/state/tests/util.spec.js
@@ -1,32 +1,72 @@
-const { createSocketEndpoint, createAuthUrl, createDeepLink, doSignedRequest, getUpdater } = require('../lib/util');
+require('node-localstorage/register');
 
-describe('createSocketEndpoint', () => {
-  test('should return a valid endpoint', () => {
-    const endpoint = createSocketEndpoint('https://www.arcblock.io');
-    expect(endpoint).toBe('wss://www.arcblock.io');
+const { createSocketEndpoint, createApiUrl, createDeepLink, doSignedRequest, getUpdater } = require('../lib/util');
+
+describe('Util', () => {
+  beforeAll(() => {
+    global.window = {
+      location: new URL('https://www.arcblock.io'),
+    };
   });
-});
 
-describe('createAuthUrl', () => {
-  test('should work as expected', () => {
-    expect(true).toBe(true);
+  describe('createSocketEndpoint', () => {
+    test('should return socket endpoint for absolute', () => {
+      expect(() => createSocketEndpoint()).toThrow();
+      const endpoint = createSocketEndpoint('https://www.arcblock.io');
+      expect(endpoint).toBe('wss://www.arcblock.io');
+    });
+
+    test('should return socket endpoint for relative', () => {
+      const endpoint = createSocketEndpoint('/api/connect/relay');
+      expect(endpoint).toBe('wss://www.arcblock.io/api/connect/relay');
+    });
   });
-});
 
-describe('createDeepLink', () => {
-  test('should work as expected', () => {
-    expect(true).toBe(true);
+  describe('createApiUrl', () => {
+    test('should throw on invalid param ', () => {
+      expect(() => createApiUrl('https://www.arcblock.io', '', '/auth')).toThrow();
+      expect(() => createApiUrl('', '/api/connect/relay', '/auth')).toThrow();
+    });
+
+    test('should return valid url for absolute', () => {
+      expect(createApiUrl('https://www.arcblock.io/api/connect/relay', 'abcd', '/auth')).toEqual(
+        'https://www.arcblock.io/api/connect/relay/auth?sid=abcd'
+      );
+    });
+
+    test('should return valid url for absolute', () => {
+      expect(createApiUrl('/api/connect/relay', 'abcd', '/auth')).toEqual(
+        'https://www.arcblock.io/api/connect/relay/auth?sid=abcd'
+      );
+      expect(createApiUrl('/api/connect/relay', 'abcd', '/session')).toEqual(
+        'https://www.arcblock.io/api/connect/relay/session?sid=abcd'
+      );
+    });
   });
-});
 
-describe('doSignedRequest', () => {
-  test('should work as expected', () => {
-    expect(true).toBe(true);
+  describe('createDeepLink', () => {
+    test('should work as expected', () => {
+      expect(createDeepLink('https://www.arcblock.io/api/connect/relay', 'abcd')).toEqual(
+        'https://abtwallet.io/i/?action=requestAuth&url=https%253A%252F%252Fwww.arcblock.io%252Fapi%252Fconnect%252Frelay%252Fauth%253Fsid%253Dabcd'
+      );
+    });
   });
-});
 
-describe('getUpdater', () => {
-  test('should work as expected', () => {
-    expect(true).toBe(true);
+  describe('doSignedRequest', () => {
+    test('should work as expected', () => {
+      expect(typeof doSignedRequest).toBe('function');
+    });
+  });
+
+  describe('getUpdater', () => {
+    test('should work as expected', () => {
+      expect(typeof getUpdater).toBe('function');
+
+      const updater = getUpdater();
+      expect(updater.publicKey).toBeTruthy();
+      expect(updater.secretKey).toBeTruthy();
+      expect(typeof updater.sign).toBe('function');
+      expect(typeof updater.verify).toBe('function');
+    });
   });
 });
