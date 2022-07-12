5b341f2cbd4acb569a6090d04e4c6f2d29761efe
wangshijun <wangshijun2010@gmail.com>
2022-07-13 06:35:41 +0800

    chore: release version

--- .makefiles/bump_blocklet_version.sh
+++ .makefiles/bump_blocklet_version.sh
@@ -1,19 +1,7 @@
 NEW_VERSION=$(cat version | awk '{$1=$1;print}')
 
-cd apps/asset-chain-api && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
-echo "bump asset chain api to version $NEW_VERSION"
+cd relay/server && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
+echo "bump relay server to version $NEW_VERSION"
 
-cd apps/asset-chain && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
-echo "bump asset chain to version $NEW_VERSION"
-
-cd apps/fs-chain-api && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
-echo "bump fs chain api to version $NEW_VERSION"
-
-cd apps/fs-chain-manager && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
-echo "bump fs chain manager to version $NEW_VERSION"
-
-cd apps/qldb-chain-api && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
-echo "bump qldb chain api to version $NEW_VERSION"
-
-cd apps/qldb-chain-manager && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
-echo "bump qldb chain manager to version $NEW_VERSION"
+cd ux/react && blocklet version $NEW_VERSION && git add blocklet.yml && cd ../../
+echo "bump react storybook to version $NEW_VERSION"
---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 2.0.1 (July 13, 2022)
+
+- chore: initial release
