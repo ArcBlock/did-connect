85a449bdd47c23ba1b242552b38d6b2a6bcb5d38
donotlb <donotlb@gmail.com>
2022-07-20 18:26:39 +0800

    chore(storybook): enable webpack5 & addons adjustments

--- ux/react/.storybook/main.js
+++ ux/react/.storybook/main.js
@@ -1,18 +1,39 @@
+const webpack = require('webpack');
+
 module.exports = {
-  stories: ['../**/*.stories.(js|mdx)'],
+  stories: ['../**/*.stories.@(js|mdx)'],
   addons: [
-    '@storybook/addon-backgrounds',
-    '@storybook/addon-actions',
+    '@storybook/addon-essentials',
     '@storybook/addon-links',
-    '@storybook/addon-viewport',
     '@storybook/addon-storysource',
-    '@storybook/addon-docs',
   ],
+  core: {
+    builder: {
+      name: 'webpack5',
+    },
+  },
   babel: async (options) => {
-    console.log(options);
     return {
       ...options,
       // any extra options you want to set
     };
+  },
+  webpackFinal: async (config) => {
+    // 解决: 开启 webpack 5 后, 启动 storybook 报 graphql 相关错误
+    // https://github.com/aws-amplify/amplify-js/issues/7260#issuecomment-840750788
+    config.module.rules.push({
+      test: /\.m?jsx?$/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+
+    // Buffer polyfill
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+      }),
+    );
+    return config;
   },
 };
