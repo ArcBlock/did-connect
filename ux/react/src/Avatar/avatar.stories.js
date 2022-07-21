62a9d14f8fb42660b728151305fa8a4715b4eec8
donotlb <donotlb@gmail.com>
2022-07-21 17:02:28 +0800

    chore: refactor avatar stories

---
+++ ux/react/src/Avatar/avatar.stories.js
@@ -0,0 +1,41 @@
+import Avatar from '.';
+import Basic from './demo/basic';
+import ResponsiveDidMotif from './demo/responsive-did-motif';
+import EthBlockies from './demo/eth-blockies';
+
+const dids = {
+  'z1YXMb8Souf2u8zVwWzexSNiD5Te7XGS313 (account)': 'z1YXMb8Souf2u8zVwWzexSNiD5Te7XGS313',
+  'zNKeLKixvCM32TkVM1zmRDdAU3bvm3dTtAcM (application)': 'zNKeLKixvCM32TkVM1zmRDdAU3bvm3dTtAcM',
+  'zjdj2tvjvLVF9S3eFaaX5xEEDuwLu6qdQk2t (asset)': 'zjdj2tvjvLVF9S3eFaaX5xEEDuwLu6qdQk2t',
+  'z35n6X6rDp8rWWCGSiXZgvd42bixdLULmX8oX (token)': 'z35n6X6rDp8rWWCGSiXZgvd42bixdLULmX8oX',
+  'invalid-did': '',
+};
+
+const argTypes = {
+  size: { control: { type: 'number', min: 20, max: 100, step: 4 }, defaultValue: 44 },
+  did: {
+    mapping: dids,
+    options: Object.keys(dids),
+    control: 'select',
+    defaultValue: 'z1YXMb8Souf2u8zVwWzexSNiD5Te7XGS313',
+  },
+  variant: { control: 'select', options: ['rounded', 'circle'] },
+  shape: { control: 'select', options: ['circle', 'hexagon', 'square', 'rectangle'] },
+  src: { control: 'select', options: ['https://picsum.photos/200/100'] },
+};
+
+export default {
+  title: 'Avatar',
+  component: Avatar,
+  argTypes,
+};
+
+ResponsiveDidMotif.parameters = {
+  controls: { exclude: Object.keys(argTypes) },
+};
+
+EthBlockies.parameters = {
+  controls: { exclude: Object.keys(argTypes) },
+};
+
+export { Basic, ResponsiveDidMotif, EthBlockies };
---
+++ ux/react/src/Avatar/demo/responsive-did-motif.js
@@ -0,0 +1,19 @@
+/* eslint-disable react/jsx-filename-extension */
+import Avatar from '..';
+
+export default function Demo(props) {
+  return (
+    <div
+      style={{
+        width: 200,
+        height: 200,
+        padding: 4,
+        border: '1px solid #eee',
+        overflow: 'hidden',
+        resize: 'both',
+      }}
+      {...props}>
+      <Avatar did="z1YXMb8Souf2u8zVwWzexSNiD5Te7XGS313" responsive />
+    </div>
+  );
+}
