50bc04c51228155b0a867f80214918f6982acd25
wangshijun <wangshijun2010@gmail.com>
2022-07-22 11:45:15 +0800

    chore: smaller storybook bundle

--- ux/react/.storybook/preview.js
+++ ux/react/.storybook/preview.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { addParameters } from '@storybook/react';
 import { ThemeProvider } from '../src/Theme';
 
-addParameters({
+export const parameters = {
   options: {
     isFullscreen: false,
     showNav: true,
@@ -13,17 +12,14 @@ addParameters({
     isToolshown: true,
     selectedPanel: 'storysource',
   },
-  controls: { hideNoControlsWarning: true },
-});
-
-addParameters({
   backgrounds: {
     values: [
       { name: 'light', value: '#f8f8f7', default: true },
       { name: 'dark', value: '#222222' },
     ],
   },
-});
+  controls: { hideNoControlsWarning: true },
+};
 
 // - 为每一个 story 包裹 ThemeProvider
 // - <StoryFn /> 解决在 story 中使用 useTheme 的问题
